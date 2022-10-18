81e90376e82ee7eddefdfa47068196e9c2e99265
ilya <bretnwood@gmail.com>
2022-10-18 15:38:39 +0300

    routeprocessor: +distribution

--- contracts/RouteProcessor.sol
+++ contracts/RouteProcessor.sol
@@ -9,8 +9,9 @@ import "../interfaces/IPool.sol";
 import "hardhat/console.sol";
 
 
+// TODO: to add bento liquidity limits because of strategies
 contract RouteProcessor {
-  IBentoBoxMinimal BentoBox;
+  IBentoBoxMinimal immutable BentoBox;
 
   constructor(address _BentoBox) {
     BentoBox = IBentoBoxMinimal(_BentoBox);
@@ -37,22 +38,32 @@ contract RouteProcessor {
         uint transferAmount;
         (transferAmount, position) = transferERC20Amount(tokenIn, route, position + 1);
         amountInAcc += transferAmount;
-
       } else if (commandCode == 2) { // send ERC20 tokens from this router to an address
         position = sendERC20Share(route, position + 1);
+      } else if (commandCode == 3) { // distribute ERC20 tokens from msg.sender to an address
+        uint transferAmount;
+        (transferAmount, position) = distributeERC20Amounts(tokenIn, route, position + 1);
+        amountInAcc += transferAmount;
+      } else if (commandCode == 4) { // distribute ERC20 tokens from this router to an address
+        position = distributeERC20Shares(route, position + 1);
 
       } else if (commandCode == 10) { // Sushi/Uniswap pool swap
         (, position) = swapUniswapPool(route, position + 1);
 
       } else if (commandCode == 20) {
-        (, position) = bentoDepositAmountFromSender(tokenIn, route, position + 1);
-        
+        (, position) = bentoDepositAmountFromSender(tokenIn, route, position + 1);        
       } else if (commandCode == 21) {
         position = swapTrident(route, position + 1);
       } else if (commandCode == 22) {
         position = bentoSend(route, position + 1);
       } else if (commandCode == 23) {
         position = bentoWithdrawShare(route, position + 1);
+      } else if (commandCode == 24) { // distribute Bento tokens from msg.sender to an address
+        uint transferAmount;
+        (transferAmount, position) = distributeBentoShares(tokenIn, route, position + 1);
+        amountInAcc += transferAmount;
+      } else if (commandCode == 25) {
+        position = distributeBentoPortions(route, position + 1);
 
       } else revert("Unknown command code");
     }
@@ -201,4 +212,113 @@ contract RouteProcessor {
     IUniswapV2Pair(pool).swap(amount0Out, amount1Out, to, new bytes(0));
   }
 
+  // Distributes input ERC20 tokens from msg.sender to addresses. Tokens should be approved
+  // Expected to be launched for initial liquidity distribution from user to pools, so we know exact amounts
+  function distributeERC20Amounts(address token, bytes memory route, uint position) 
+    private returns (uint amountTotal, uint positionAfter) {
+    uint8 num;
+    assembly {
+      route := add(route, add(position, 1))
+      num := mload(route)
+    }
+
+    amountTotal = 0;
+    address to;  
+    uint amount;  
+    for (uint i = 0; i < num; ++i) {
+      assembly {
+        to := mload(add(route, 20))
+        route := add(route, 52)
+        amount := mload(route)
+        amountTotal := add(amountTotal, amount)
+      }
+      IERC20(token).transferFrom(msg.sender, to, amount);
+    }
+    positionAfter = position + 1 + num*52;
+  }
+
+  // Distributes input Bento tokens from msg.sender to addresses. Tokens should be approved
+  // Expected to be launched for initial liquidity distribution from user to pools, so we know exact amounts
+  function distributeBentoShares(address token, bytes memory route, uint position) 
+    private returns (uint sharesTotal, uint positionAfter) {
+    uint8 num;
+    assembly {
+      route := add(route, add(position, 1))
+      num := mload(route)
+    }
+
+    sharesTotal = 0;
+    address to;  
+    uint share;  
+    for (uint i = 0; i < num; ++i) {
+      assembly {
+        to := mload(add(route, 20))
+        route := add(route, 52)
+        share := mload(route)
+        sharesTotal := add(sharesTotal, share)
+      }
+      BentoBox.transfer(token, msg.sender, to, share);
+    }
+    positionAfter = position + 1 + num*52;
+  }
+
+  // Distribute ERC20 tokens from this routeProcessor to addresses. 
+  // Quantity for sending is determined by share in 1/65535.
+  // During routing we can't predict in advance the actual value of internal swaps because of slippage,
+  // so we have to work with shares - not fixed amounts
+  function distributeERC20Shares(bytes memory route, uint position) 
+    private returns (uint positionAfter) {
+    uint8 num;
+    address token;
+    assembly {
+      route := add(route, position)
+      token := mload(add(route, 20))
+      route := add(route, 21)
+      num := mload(route)
+    }
+    uint amountTotal = IERC20(token).balanceOf(address(this));
+
+    address to;  
+    uint amount;  
+    for (uint i = 0; i < num; ++i) {
+      assembly {
+        to := mload(add(route, 20))
+        route := add(route, 22)
+        let share := and(mload(route), 0xffff)
+        amount := div(mul(amountTotal, share), 65535)
+      }
+      IERC20(token).transfer(to, amount);
+    }
+    positionAfter = position + 21 + num*22;
+  }
+
+  // Distribute Bento tokens from this routeProcessor to addresses. 
+  // Quantity for sending is determined by portions in 1/65535.
+  // During routing we can't predict in advance the actual value of internal swaps because of slippage,
+  // so we have to work with portions - not fixed amounts
+  function distributeBentoPortions(bytes memory route, uint position) 
+    private returns (uint positionAfter) {
+    uint8 num;
+    address token;
+    assembly {
+      route := add(route, position)
+      token := mload(add(route, 20))
+      route := add(route, 21)
+      num := mload(route)
+    }
+    uint amountTotal = BentoBox.balanceOf(token, address(this));
+
+    address to;  
+    uint amount;  
+    for (uint i = 0; i < num; ++i) {
+      assembly {
+        to := mload(add(route, 20))
+        route := add(route, 22)
+        let share := and(mload(route), 0xffff)
+        amount := div(mul(amountTotal, share), 65535)
+      }
+      BentoBox.transfer(token, address(this), to, amount);
+    }
+    positionAfter = position + 21 + num*22;
+  }
 }
